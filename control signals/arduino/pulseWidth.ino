7d7fc3d6ad0e099137b5ee781b77cbe9eb59b098
Isaiah P <124836428+w1ggle@users.noreply.github.com>
2024-02-25 10:23:21 -0500

    arduino code

---
+++ control signals/arduino/pulseWidth.ino
@@ -0,0 +1,32 @@
+int controlPin = 8;
+int frequency = 50; //in hertz
+int pulseMin = 500; //in microseconds
+int pulseMax = 2500; //in microseconds
+int angleMin = 0; //in degrees
+int angleMax = 270; //in degrees
+int pulse;
+int angle;
+int period;
+
+void setup(){
+  pinMode(controlPin, OUTPUT);
+  period = 1.0/frequency * 1000000; //convert to microseconds
+
+  //Serial.begin(9600);
+}
+
+
+void loop(){
+
+  pulse = 0;
+  while(pulse < pulseMax){
+    angle = map(pulse, pulseMin, pulseMax, angleMin, angleMax);
+    //Serial.println(angle);
+    digitalWrite(controlPin, HIGH);
+    delayMicroseconds(pulse);
+    digitalWrite(controlPin, LOW);
+    delayMicroseconds(period - pulse);
+    pulse++;
+  }
+
+}
---
+++ control signals/arduino/pwm.ino
@@ -0,0 +1,18 @@
+int pwmEmulatorPin = 8;
+int maxRange = 2500;
+int dutyCyclePercent = 20;
+float activeTime = 0;
+float nonActiveTime = 0;
+
+void setup(){
+  pinMode(pwmEmulatorPin, OUTPUT);
+  activeTime = maxRange * (dutyCyclePercent / 100.0);
+  nonActiveTime = maxRange * ((100 - dutyCyclePercent) / 100.0);
+}
+
+void loop(){
+  digitalWrite(pwmEmulatorPin, HIGH);
+  delayMicroseconds(activeTime);
+  digitalWrite(pwmEmulatorPin, LOW);
+  delayMicroseconds(nonActiveTime);
+}
