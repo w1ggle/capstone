0304228dc7c3e352564e6c5192bc1d060f53e8ba
Isaiah P <124836428+w1ggle@users.noreply.github.com>
2024-04-15 13:13:58 -0400

    more control keyboard

--- control signals/UART/jetson (master)/carControlKEYBOARD.py
+++ control signals/UART/jetson (master)/carControlKEYBOARD.py
@@ -6,16 +6,18 @@ import arduinoControl as ac
 while True:  #polling; ctrl+C to stop
 
     try:  # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed('w'): 
+        if keyboard.is_pressed('i'): 
             ac.forward()
-        elif keyboard.is_pressed('s'):
+        elif keyboard.is_pressed('k'):
             ac.back()
         elif keyboard.is_pressed('a'):
             ac.left()
+        elif keyboard.is_pressed('s'):
+            ac.straight()
         elif keyboard.is_pressed('d'):
             ac.right()
-        elif keyboard.is_pressed('r'):
-            ac.send_command(2, 1500)
-            ac.send_command(1, 1500)
+        else:
+            ac.stop()
+            
     except:
         break
