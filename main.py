d0c4c84cb0e69db34a780f5e889e4631ddfed170
Isaiah P <124836428+w1ggle@users.noreply.github.com>
2024-04-15 12:59:14 -0400

    simplifiying loop

--- main.py
+++ main.py
@@ -4,30 +4,28 @@
 #import arduinoControl as ac
 #import motionPlanning as mp 
 
-finished = False
-PARKED = True #get variable from mp and remove this when ready
+PARKED = False #set false so we stay in while loop
 
-#load model (which will open tf session)
+#load model (which will open tf session) i think as long as we dont end code, session is still in play
 #model_path = './models/trt_frozen_graphtf2FP32.pb'
 #model = sementic_segmentation.avm_ss(model_path)
 
 #wait for keyboard press, allows us to disconnect monitor and put the car on ground
 input("disconnect car, place on ground, press enter when ready")
 
-while finished is not True:
+while PARKED is not True:
     # put AVM stitching code here *(make it into a file that can be imported)
     #surround = cs.runBEV() #AVM stitched and cropped output
     
     #run the model on on the avm output
     #labeledSurround = model.run(surround)
     
-    if PARKED is True:
-        #call finished function here. Will wiggle wheels for visual confirmation (will have monitor unplugged so cant see terminal if done)
-        #finished = ac.finish()
-        finished = True
-    else:
-        #run the movement commands
-        # PARKED = mp.movement(labeledSurround) #move forward, back left right etc. return true if parked at right spot
-        print("continuing navigation")
+
     
+    #run the movement commands
+    # PARKED = mp.movement(labeledSurround) #move forward, back left right etc. return true if parked at right spot
+    print("continuing navigation")
+
+#call finished function here. Will wiggle wheels for visual confirmation (wont have monitor unplugged so cant see terminal if done)
+#ac.finish()
 print("done!")
